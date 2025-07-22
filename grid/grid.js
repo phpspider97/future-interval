893ddf745bd3b8d8450f2ad7f250939ea3d1dbe8
neel <neelbhardwaj97@gmail.com>
2025-07-22 19:38:28 +0530

    update

--- grid/grid.js
+++ grid/grid.js
@@ -385,7 +385,7 @@ async function createOrder(bid_type,order_price){
         const bodyParams = {
             product_id : bitcoin_product_id,
             product_symbol : "BTCUSD",
-            size : 4, 
+            size : 5, 
             side : bid_type,   
             order_type : "limit_order",
             limit_price : order_price
